5fcbf69693125c8450e18b9f5bab5468bf0e5d0e
hanchen <hanchenjhc@gmail.com>
2020-05-24 11:43:54 -0500

    add left vs right ratio control

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 function App() {
     const [showLeft, setShowLeft] = useState(false);
     const [showRight, setShowRight] = useState(false);
     const [showControl, setShowControl] = useState(true);
     const [switchFrequency, setSwitchFrequency] = useState(1000);
     const [intervalId, setIntervalId] = useState();
+    const [leftPercentage, setLeftPercentage] = useState(50);
 
     function showLeftOrRight() {
-        if (Math.floor(Math.random() * 2 % 2) === 0) {
+        let randomInt = getRandomInt(100);
+        if (randomInt < leftPercentage) {
             setShowLeft(true);
             setShowRight(false);
         } else {
@@ -28,7 +34,7 @@ function App() {
         setIntervalId(setInterval(() => {
             showLeftOrRight();
         }, switchFrequency));
-    }, [switchFrequency]);
+    }, [switchFrequency, leftPercentage]);
 
     function changeSwitchFrequency(event: React.ChangeEvent<HTMLInputElement>) {
         setSwitchFrequency(Number(event.target.value));
@@ -38,16 +44,30 @@ function App() {
         <div className="App">
             {showControl?
             <section>
+                <div>
                 <label>Milliseconds each change: </label>
                 <input
                     value={switchFrequency}
                     type="number"
                     onChange={changeSwitchFrequency}
                 />
+                </div>
+                <div>
+                    <label>left vs right ratio: </label>
+                    <input
+                        type="range"
+                        min="1"
+                        max="100"
+                        value={leftPercentage.toString()}
+                        onChange={(event) => {
+                            setLeftPercentage(Number(event.target.value));
+                        }}
+                    />
+                </div>
             </section>: null}
             <section>
-                <label>Hide control</label>
-                <input type='checkbox' onChange={(event) =>{setShowControl((prevState => !prevState))}} />
+                <label htmlFor="hide-control">Hide control</label>
+                <input id="hide-control" type='checkbox' onChange={() =>{setShowControl(!showControl)}} />
             </section>
             <section id="display-area">
                 <section>
