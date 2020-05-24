cac642a1af8367b2668cda5e3ca5e918d4991643
hanchen <hanchenjhc@gmail.com>
2020-05-23 22:34:35 -0500

    Initial commit.
    + display circle on left or right side of display area.
    + switch every 1000 ms. clear after 700ms showing the circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showLeft, setShowLeft] = useState(false);
+    const [showRight, setShowRight] = useState(false);
+
+    function showLeftOrRight() {
+        if (Math.floor(Math.random() * 2 % 2) === 0) {
+            setShowLeft(true);
+            setShowRight(false);
+        } else {
+            setShowLeft(false);
+            setShowRight(true);
+        }
+
+        setTimeout(() => {
+            setShowLeft(false);
+            setShowRight(false);
+        }, 700);
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            showLeftOrRight();
+        }, 1000);
+    }, []);
+
+    return (
+        <div className="App">
+            <section id="display-area">
+                <section>
+                    {showLeft ? <span className="circle"/> : null}
+                </section>
+                <section>
+                    {showRight ? <span className="circle"/> : null}
+                </section>
+            </section>
+        </div>
+    );
 }
 
 export default App;
